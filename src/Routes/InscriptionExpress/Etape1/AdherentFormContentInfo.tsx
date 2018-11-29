6cdf6fdc52df4cb5d81c95e07f4bdfccdfac8869
Dadoudidou <davidviolet18@gmail.com>
2018-11-29 10:50:33 +0100

    Ajout photo

--- src/Routes/InscriptionExpress/Etape1/AdherentFormContentInfo.tsx
+++ src/Routes/InscriptionExpress/Etape1/AdherentFormContentInfo.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import Datas, { adherent, IExpress } from "../../../Datas";
 import { AccountCircleOutlined } from "@material-ui/icons"
-import { Typography, TextField, Grid, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { Typography, TextField, Grid, FormControl, InputLabel, Select, MenuItem, Button } from "@material-ui/core";
 import * as moment from "moment";
 import DatasIexpress from "../../../Datas/DatasIexpress";
 import GridItemForm from "../../../Components/Commons/GridItemForm";
 import AutoComplete from "../../../System/Framer/Components/AutoComplete";
 import AdherentsAutoComplete from "../../../Components/Adherents/AdherentsAutoComplete";
 import AutoSuggest from "../../../System/Framer/Components/AutoSuggest";
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 export const onUpdateAdherent = (adherent: adherent, data: IExpress) => {
     return {
@@ -31,34 +32,54 @@ export default () => {
                         <AccountCircleOutlined style={{ verticalAlign: "middle" }} /> Informations personnelles
                     </Typography>
                     <Grid container>
+
                         <GridItemForm item xs={12}>
-                            <AdherentsAutoComplete 
-                                fullWidth
-                                autoFocus
-                                label="Nom"
-                                value={adherent.nom || ""}
-                                onChange={(ev) => {
-                                    data.update(onUpdateAdherent({
-                                        ...adherent,
-                                        nom: ev.target.value
-                                    }, data.data))
-                                }}
-                                onSelect={item => data.update(onUpdateAdherent(item, data.data))}
-                            />
+                            <Grid container direction="row" style={{alignItems:"center"}}>
+                                <GridItemForm item  >
+                                    <Button style={{
+                                        width: 64, height: 64,
+                                        padding: 0, display: "block", margin: "auto",
+                                        background: "#eee"
+                                    }}>
+                                        <PhotoCamera />
+                                    </Button>
+                                </GridItemForm>
+                                <Grid item style={{flexGrow:1, paddingLeft: 16}}>
+                                    <Grid container direction="column">
+                                    <GridItemForm item xs={12}>
+                                        <AdherentsAutoComplete 
+                                            fullWidth
+                                            autoFocus
+                                            label="Nom"
+                                            value={adherent.nom || ""}
+                                            onChange={(ev) => {
+                                                data.update(onUpdateAdherent({
+                                                    ...adherent,
+                                                    nom: ev.target.value
+                                                }, data.data))
+                                            }}
+                                            onSelect={item => data.update(onUpdateAdherent(item, data.data))}
+                                        />
+                                    </GridItemForm>
+                                    <GridItemForm item xs={12}>
+                                        <TextField 
+                                            fullWidth
+                                            label="Prénom"
+                                            value={adherent.prenom || ""}
+                                            onChange={(ev) => {
+                                                data.update(onUpdateAdherent({
+                                                    ...adherent,
+                                                    prenom: ev.target.value
+                                                }, data.data))
+                                            }}
+                                        />
+                                    </GridItemForm>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
                         </GridItemForm>
-                        <GridItemForm item xs={12}>
-                            <TextField 
-                                fullWidth
-                                label="Prénom"
-                                value={adherent.prenom || ""}
-                                onChange={(ev) => {
-                                    data.update(onUpdateAdherent({
-                                        ...adherent,
-                                        prenom: ev.target.value
-                                    }, data.data))
-                                }}
-                            />
-                        </GridItemForm>
+                        
+                        
                         <GridItemForm item xs={12}>
                             <TextField 
                                 fullWidth
